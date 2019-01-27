8cb0ab927c54ec6eec56ca775be034a75b722218
masterlink950 <masterlink950@gmail.com>
2019-01-26 22:17:41 -0500

    More finely defined hexagon sizing and placement

--- src/components/Board/Hexagon.js
+++ src/components/Board/Hexagon.js
@@ -39,10 +39,10 @@ class Hexagon extends PureComponent {
     const width = size * 2;
     const height = size / 0.57471264367;
 
-    const yPosition = y * (height) / 2.02841539;
+    const yPosition = y * height / 2.094415;
     const xPosition = isOffsetColumn
-      ? x * width * 1.4859 + width / 1.34623
-      : x * width * 1.4859;
+      ? x * width * 1.43885 + width / 1.39
+      : x * width * 1.43885;
 
     return (
       <HexagonContainer
@@ -55,7 +55,7 @@ class Hexagon extends PureComponent {
         <svg
           height={height}
           version="1.1"
-          viewBox="-1 0 202 173.20508075688772"
+          viewBox="-4.25 0 208.5 173.20508075688772"
           width={width}
           xmlns="http://www.w3.org/2000/svg"
         >
@@ -69,12 +69,12 @@ class Hexagon extends PureComponent {
                 onClick={this.handleClick}
               />
             </g>
-            <line loc="t  " stroke={this.edgeColor('T')}  strokeWidth="7" x1="153" x2="47.7" y1="1" y2="1"/>
-            <line loc="t-r" stroke={this.edgeColor('TR')} strokeWidth="7" transform="rotate(60.14806365966797 175.34083557128906,43.948944091796896)" x1="225.608381" x2="125.073283" y1="43.948944" y2="43.948944"/>
-            <line loc="b-r" stroke={this.edgeColor('BR')} strokeWidth="7" transform="rotate(-60.00247573852539 175.4222564697266,130.13467407226562)" x1="225.333754" x2="125.510758" y1="130.134664" y2="130.134664"/>
-            <line loc="b  " stroke={this.edgeColor('B')}  strokeWidth="7" x1="153" x2="47.7" y1="172" y2="172"/>
-            <line loc="b-l" stroke={this.edgeColor('BL')} strokeWidth="7" transform="rotate(60 25.35009193420411,129.74034118652344)" x1="75.550259" x2="-24.850086" y1="129.740341" y2="129.740341"/>
-            <line loc="t-l" stroke={this.edgeColor('TL')} strokeWidth="7" transform="rotate(-60 25.000000000000007,43.500019073486335)" x1="75" x2="-25" y1="43.833339" y2="43.833339"/>
+            <line loc="t  " stroke={this.edgeColor('T')}  strokeWidth="7" x1="151.95" x2="47.99" y1="0" y2="0"/>
+            <line loc="t-r" stroke={this.edgeColor('TR')} strokeWidth="7" transform="rotate(60.14806365966797 175.34083557128906,43.948944091796896)" x1="226.67" x2="122.55" y1="43.8" y2="44.1"/>
+            <line loc="b-r" stroke={this.edgeColor('BR')} strokeWidth="7" transform="rotate(-60.00247573852539 175.4222564697266,130.13467407226562)" x1="227.4" x2="123.45" y1="129.65" y2="129.65"/>
+            <line loc="b  " stroke={this.edgeColor('B')}  strokeWidth="7" x1="152.08" x2="48" y1="173.2" y2="173.2"/>
+            <line loc="b-l" stroke={this.edgeColor('BL')} strokeWidth="7" transform="rotate(60 25.35009193420411,129.74034118652344)" x1="77.35" x2="-26.7" y1="130.1" y2="130.1"/>
+            <line loc="t-l" stroke={this.edgeColor('TL')} strokeWidth="7" transform="rotate(-60 25.000000000000007,43.500019073486335)" x1="77.19" x2="-26.83" y1="43.4" y2="43.4"/>
           </g>
         </svg>
       </HexagonContainer>
