148badabe205ff7476238dfaf8fef7a065088211
masterlink950 <masterlink950@gmail.com>
2019-01-28 18:51:52 -0500

    Make this method faster in MS

--- src/utils/hexMath/grid.js
+++ src/utils/hexMath/grid.js
@@ -4,14 +4,18 @@ export const getGridHexes = (gridData, modifyHexCallback) => {
     xArray.forEach((hexData, xIndex) => {
       let newHex = hexData;
       if (modifyHexCallback) {
-        newHex = modifyHexCallback({x: xIndex, y: yIndex}, hexData);
+        hexes.push({
+          gridSize: {yLength: gridData.length, xLength: xArray.length},
+          gridCoords: {x: xIndex, y: yIndex},
+          hexData: modifyHexCallback({x: xIndex, y: yIndex}, hexData),
+        });
+      } else {
+        hexes.push({
+          gridSize: {yLength: gridData.length, xLength: xArray.length},
+          gridCoords: {x: xIndex, y: yIndex},
+          hexData: newHex,
+        });
       }
-
-      hexes.push({
-        gridSize: {yLength: gridData.length, xLength: xArray.length},
-        gridCoords: {x: xIndex, y: yIndex},
-        hexData: newHex,
-      });
     });
   });
   return hexes;
