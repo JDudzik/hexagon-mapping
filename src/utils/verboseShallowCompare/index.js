bcc87857a40db32ea32d1a8e85b1828608b62e99
masterlink950 <masterlink950@gmail.com>
2019-02-15 00:05:25 -0500

    Improve shallow compare results display

--- src/utils/verboseShallowCompare/index.js
+++ src/utils/verboseShallowCompare/index.js
@@ -4,15 +4,23 @@ const debugShallowCompare = (obj1, obj2) => {
   const lengthSame = Object.keys(obj1).length === Object.keys(obj2).length;
   const propsMatch = Object.keys(obj1).every(key => {
     const isSameValue = obj2.hasOwnProperty(key) && obj1[key] === obj2[key];
-    if (!isSameValue) {
-      console.group();
-      const compareAnnouncement = `--- Shallow Compare ---`;
-			console.log('%c' + compareAnnouncement, 'color:red; font-weight:bold;');
-      console.log('%c' + key, 'font-weight:bold;');
-      console.groupEnd();
-    }
     return isSameValue;
   });
+
+  if (!propsMatch) {
+    console.group();
+    const compareAnnouncement = `--- Shallow Compare ---`;
+    console.log('%c' + compareAnnouncement, 'color:red; font-weight:bold;');
+
+    Object.keys(obj1).forEach(key => {
+      const isSameValue = obj2.hasOwnProperty(key) && obj1[key] === obj2[key];
+      if (!isSameValue) {
+        console.log('%c' + key, 'font-weight:bold;');
+      }
+    })
+
+    console.groupEnd();
+  }
 
   return lengthSame && propsMatch;
 };
